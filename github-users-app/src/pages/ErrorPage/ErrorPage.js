ad123888892bcf35744d2983a806ab8e913bdfdd
Guilherme Henrique Mota Alfredo <guilhermehmota12@gmail.com>
2021-06-27 13:15:07 -0300

    página de erro pronta

--- github-users-app/src/pages/ErrorPage/ErrorPage.js
+++ github-users-app/src/pages/ErrorPage/ErrorPage.js
@@ -1,10 +1,21 @@
 import React from "react"
+import { MainContainer } from "../../styled"
+import octoCat from "../../img/octoCat.png"
+import {OctocatImg, Title} from "./styled"
+import Button from '@material-ui/core/Button';
+import { useHistory } from "react-router-dom";
+import { goToInitialPage } from "../../routes/coordinator";
 
 const ErrorPage = () => {
+
+    const history = useHistory()
+
     return(
-        <div>
-            <h1>ErrorPage</h1>
-        </div>
+        <MainContainer>
+            <Title>Ops! Esta página não existe =(</Title>
+            <OctocatImg src={octoCat}></OctocatImg>
+            <Button type="submit" variant="contained" color="primary" onClick={() => goToInitialPage(history)}>voltar a para página inicial</Button>
+        </MainContainer>
     )
 }
 
